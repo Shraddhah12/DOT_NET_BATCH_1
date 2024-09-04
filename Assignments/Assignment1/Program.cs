9321254676e91dd01fa3a6afd34efc945f2623ba
Shraddhah12 <shraddhaholkar12@gmail.com>
2024-09-04 18:07:31 +0530

    Namespace Assignment

---
+++ Assignments/Assignment1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DataType;
+class MainClass
+{
+    public static void Main(String[] args)
+    {
+    _INT  a= new _INT(10);
+    _INT b= new _INT(20);
+    Console.WriteLine(a.addition(a,b));
+    Console.WriteLine(b.substraction(a,b));
+
+      _FLOAT c = new _FLOAT(10);
+      _FLOAT d = new _FLOAT(20);
+
+    Console.WriteLine(c.addition(c,d));
+    Console.WriteLine(d.substraction(c,d));
+    }
+    
+}
