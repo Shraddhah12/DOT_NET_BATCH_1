21248fa67908c442b00aad26392580be680034f3
Shraddhah12 <shraddhaholkar12@gmail.com>
2024-09-04 23:38:07 +0530

    Add files via upload

---
+++ Assignment/datatype/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace dataType
+{
+    public class DataType
+    {
+        public static void Main (String[] args)
+        {
+            int num =10;
+            float f = 12.55f;
+            double d = 123.345;
+            bool b = false;
+
+            Console.WriteLine(num);
+            Console.WriteLine(f.ToString());
+            
+            Console.WriteLine(d);
+            
+            Console.WriteLine(b.ToString());
+
+        }
+    }
+
+}
