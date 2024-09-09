dd7fad62b5881d147c2b7058429e020935f5ef2c
Shraddhah12 <shraddhaholkar12@gmail.com>
2024-09-09 23:02:17 +0530

    unorderdDynamicArray

---
+++ dynamicArray/unOrderedDynamicArray.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace DynamicArrays
+{
+    public class UnorderedIntDynamicArray : IntDynamicArray
+    {
+        #region Constructor
+
+        public UnorderedIntDynamicArray()
+            :base()
+        {
+        }
+        public override void Add(int item)
+        {
+            if(count == items.Length)
+                Expand();
+            
+            items[count] = item;
+            count++;
+        }
+
+        public override bool Remove(int item)
+        {
+            int itemLocation = IndexOf(item);
+            if(itemLocation == -1)
+                return false;
+            else
+            {
+                items[itemLocation] = items[count - 1];
+                count--;
+                return true;
+            }
+        }
+
+        public override int IndexOf(int item)
+        {
+            for(int i = 0; i < count; i++)
+            {
+                if(items[i] == item)
+                    return i;
+            }
+            return -1;  
+        }
+    }
+}
